8b12d5bfa7b7cdb9e9488f9f442065b8203a3ceb
Neil Walker <neil@wynded.co.uk>
2020-11-06 16:19:30 +0000

    Allow host to be specified on the command line

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #! /bin/sh
 
+host=$1
+
+if [ "$host" ]; then
+  host="--host $host"
+fi
+
 amixer sset Headphone 100%
-python3 $(dirname $0)/main.py --hide-mouse-pointer --no-close-button
+python3 $(dirname $0)/main.py --hide-mouse-pointer --no-close-button $host
