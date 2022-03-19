9cf1aadd292fca1acb327c8020b41de09bfc2dff
Neil Walker <neil@wynded.co.uk>
2022-03-19 11:58:11 +0000

    Add scripts to run tests and capture coverage

---
+++ run_tests.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+pytest --cov=apiclient --cov-report=xml
--- sonarqube.sh
+++ sonarqube.sh
@@ -4,4 +4,5 @@ sonar-scanner  \
   -Dsonar.projectKey=pijuui \
   -Dsonar.sources=./src \
   -Dsonar.host.url=http://localhost:9000 \
+  -Dsonar.python.coverage.reportPaths=coverage.xml \
   -Dsonar.login=3377f7f1b54bd9473619970a1d5e1bf81f09187a
