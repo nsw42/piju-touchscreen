38d4c434bd9e09ece7e329c35c24ecb84598094d
Neil Walker <neil@wynded.co.uk>
2022-03-19 11:58:42 +0000

    Add coverage for api_client.get_artwork

--- test/test_api_client.py
+++ test/test_api_client.py
@@ -11,7 +11,8 @@ sys.path.append('..')
 import apiclient  # noqa: E402
 
 
-FakeResponse = namedtuple('FakeResponse', 'ok status_code text json')
+FakeTextResponse = namedtuple('FakeTextResponse', 'ok status_code text json')
+FakeBinaryResponse = namedtuple('FakeBinaryResponse', 'ok status_code text content')
 
 logging.disable(logging.CRITICAL)  # disable logging from the apiclient code
 
@@ -27,7 +28,7 @@ class TestGetCurrentState(unittest.TestCase):
 
     @patch('apiclient.requests.get', side_effect=[
         requests.exceptions.ConnectionError,
-        FakeResponse(ok=False, status_code=500, text='', json=None)
+        FakeTextResponse(ok=False, status_code=500, text='', json=None)
     ])
     def test_successful_connection_clears_flag(self, mock_requests_get):
         client = apiclient.ApiClient('http://address/')
@@ -42,28 +43,31 @@ class TestGetCurrentState(unittest.TestCase):
         state = client.get_current_state()
         self.assertFalse(client.connection_error)
 
-    @patch('apiclient.requests.get', return_value=FakeResponse(ok=False, status_code=500, text='', json=None))
+    @patch('apiclient.requests.get',
+           return_value=FakeTextResponse(ok=False, status_code=500, text='', json=None))
     def test_non_ok_response(self, mock_requests_get):
         client = apiclient.ApiClient('http://address/')
         state = client.get_current_state()
         self.assertEqual(state.status, None)
 
-    @patch('apiclient.requests.get', return_value=FakeResponse(ok=True, status_code=200, text='', json=lambda: None))
+    @patch('apiclient.requests.get',
+           return_value=FakeTextResponse(ok=True, status_code=200, text='', json=lambda: None))
     def test_non_json_repsonse(self, mock_requests_get):
         client = apiclient.ApiClient('http://address/')
         state = client.get_current_state()
         self.assertEqual(state.status, None)
 
-    @patch('apiclient.requests.get', return_value=FakeResponse(ok=True, status_code=200, text='', json=lambda: {}))
+    @patch('apiclient.requests.get',
+           return_value=FakeTextResponse(ok=True, status_code=200, text='', json=lambda: {}))
     def test_json_without_status(self, mock_requests_get):
         client = apiclient.ApiClient('http://address/')
         state = client.get_current_state()
         self.assertEqual(state.status, None)
 
     @patch('apiclient.requests.get', side_effect=[
-        FakeResponse(ok=True, status_code=200, text='', json=lambda: {'PlayerStatus': 'stopped'}),
-        FakeResponse(ok=True, status_code=200, text='', json=lambda: {'PlayerStatus': 'playing'}),
-        FakeResponse(ok=True, status_code=200, text='', json=lambda: {'PlayerStatus': 'paused'}),
+        FakeTextResponse(ok=True, status_code=200, text='', json=lambda: {'PlayerStatus': 'stopped'}),
+        FakeTextResponse(ok=True, status_code=200, text='', json=lambda: {'PlayerStatus': 'playing'}),
+        FakeTextResponse(ok=True, status_code=200, text='', json=lambda: {'PlayerStatus': 'paused'}),
     ])
     def test_status_values(self, mock_requests_get):
         client = apiclient.ApiClient('http://address/')
@@ -85,7 +89,7 @@ class TestGetArtworkInfo(unittest.TestCase):
 
     @patch('apiclient.requests.get', side_effect=[
         requests.exceptions.ConnectionError,
-        FakeResponse(ok=False, status_code=500, text='', json=None)
+        FakeTextResponse(ok=False, status_code=500, text='', json=None)
     ])
     def test_successful_connection_clears_flag(self, mock_requests_get):
         client = apiclient.ApiClient('http://address/')
@@ -100,7 +104,8 @@ class TestGetArtworkInfo(unittest.TestCase):
         state = client.get_artwork_info('/artworkinfo/27')
         self.assertFalse(client.connection_error)
 
-    @patch('apiclient.requests.get', return_value=FakeResponse(ok=False, status_code=500, text='', json=None))
+    @patch('apiclient.requests.get',
+           return_value=FakeTextResponse(ok=False, status_code=500, text='', json=None))
     def test_non_ok_response(self, mock_requests_get):
         client = apiclient.ApiClient('http://address/')
         info = client.get_artwork_info('/artworkinfo/784')
@@ -108,7 +113,8 @@ class TestGetArtworkInfo(unittest.TestCase):
         self.assertEqual(info.height, None)
         self.assertEqual(info.imageuri, None)
 
-    @patch('apiclient.requests.get', return_value=FakeResponse(ok=True, status_code=200, text='', json=lambda: None))
+    @patch('apiclient.requests.get',
+           return_value=FakeTextResponse(ok=True, status_code=200, text='', json=lambda: None))
     def test_non_json_repsonse(self, mock_requests_get):
         client = apiclient.ApiClient('http://address/')
         info = client.get_artwork_info('/artworkinfo/784')
@@ -117,10 +123,10 @@ class TestGetArtworkInfo(unittest.TestCase):
         self.assertEqual(info.imageuri, None)
 
     @patch('apiclient.requests.get',
-           return_value=FakeResponse(ok=True,
-                                     status_code=200,
-                                     text='',
-                                     json=lambda: {"width": 44, "height": 55, "image": "/artwork/12"})
+           return_value=FakeTextResponse(ok=True,
+                                         status_code=200,
+                                         text='',
+                                         json=lambda: {"width": 44, "height": 55, "image": "/artwork/12"})
            )
     def test_status_values(self, mock_requests_get):
         client = apiclient.ApiClient('http://address/')
@@ -128,6 +134,52 @@ class TestGetArtworkInfo(unittest.TestCase):
         self.assertEqual(info.width, 44)
         self.assertEqual(info.height, 55)
         self.assertEqual(info.imageuri, "/artwork/12")
+
+
+class TestGetArtwork(unittest.TestCase):
+    def test_get_none(self):
+        client = apiclient.ApiClient('http://address/')
+        artwork = client.get_artwork(None)
+        self.assertEqual(artwork, None)
+
+    @patch('apiclient.requests.get', side_effect=requests.exceptions.ConnectionError)
+    def test_connection_error_returns_none_and_sets_flag(self, mock_requests_get):
+        client = apiclient.ApiClient('http://address/')
+        self.assertFalse(client.connection_error)
+        artwork = client.get_artwork('/artwork/123')
+        self.assertEqual(artwork, None)
+        self.assertTrue(client.connection_error)
+
+    @patch('apiclient.requests.get', side_effect=[
+        requests.exceptions.ConnectionError,
+        FakeBinaryResponse(ok=False, status_code=500, text='', content=None)
+    ])
+    def test_successful_connection_clears_flag(self, mock_requests_get):
+        client = apiclient.ApiClient('http://address/')
+        self.assertFalse(client.connection_error)
+
+        # first call: sets flag
+        artwork = client.get_artwork('/artwork/123')
+        self.assertEqual(artwork, None)
+        self.assertTrue(client.connection_error)
+
+        # second call: clears flag
+        artwork = client.get_artwork('/artwork/123')
+        self.assertFalse(client.connection_error)
+
+    @patch('apiclient.requests.get',
+           return_value=FakeBinaryResponse(ok=False, status_code=500, text='', content=None))
+    def test_non_ok_response(self, mock_requests_get):
+        client = apiclient.ApiClient('http://address/')
+        artwork = client.get_artwork('/artwork/123')
+        self.assertEqual(artwork, None)
+
+    @patch('apiclient.requests.get',
+           return_value=FakeBinaryResponse(ok=True, status_code=200, text='', content='MyArtworkHere'))
+    def test_ok_response(self, mock_requests_get):
+        client = apiclient.ApiClient('http://address/')
+        artwork = client.get_artwork('/artwork/123')
+        self.assertEqual(artwork, 'MyArtworkHere')
 
 
 if __name__ == '__main__':
